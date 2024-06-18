0454db3dcc16a3902cd60b8e2bbc6eece46b1bd4
fenglekai <674640130@qq.com>
2024-06-18 11:59:30 +0800

    fix: 🐛 添加peer dependencies解决构建含有依赖问题
    vue打包后存在@vue/runtime-dom

--- internal/build/src/constants.ts
+++ internal/build/src/constants.ts
@@ -3,7 +3,10 @@ export const PKG_PREFIX = '@less-write'
 export const PKG_NAME = 'less-write'
 export const PKG_BRAND_NAME = 'Less Write'
 export const PKG_CAMELCASE_NAME = 'LessWrite'
+
 export const target = 'esnext'
+
 export const version = '0.0.0-dev.1'
 export const banner = `/*! ${PKG_BRAND_NAME} v${version} */\n`
+
 export const NODE_ENV = JSON.stringify("production")

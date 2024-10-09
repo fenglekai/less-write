99c39c79eff977a99af3ec94767b65a67b95ef51
fenglekai <674640130@qq.com>
2024-10-09 11:39:58 +0800

    feat(less-write-ui): 🎸 添加slider组件的导出声明

--- packages/less-write-ui/component.ts
+++ packages/less-write-ui/component.ts
@@ -1,6 +1,7 @@
 import { LeButton } from "@less-write/components/button";
 import { LeMap } from "@less-write/components/map";
+import { LeSlider } from "@less-write/components/slider";
 
 import type { Plugin } from "vue";
 
-export default [LeButton, LeMap] as Plugin[];
+export default [LeButton, LeMap, LeSlider] as Plugin[];
--- typings/components.d.ts
+++ typings/components.d.ts
@@ -3,7 +3,7 @@ import '@vue/runtime-core'
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     LeMap: typeof import('../packages/less-write-ui')['LeMap'],
-    useMap: typeof import('../packages/less-write-ui')['useMap'],
     LeButton: typeof import('../packages/less-write-ui')['LeButton'],
+    LeSlider: typeof import('../packages/less-write-ui')['LeSlider'],
   }
 }
